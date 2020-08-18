edfad6c6dfe07b34ffbe320495191c3d1be57de2
Mauro Dorigo-Cortes <mau.dorigo@outlook.es>
2020-08-18 17:52:06 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,15 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    
+    string = 10 * "s" + string
+    binding.pry
   else
+    
     string
   end
+  
 end
+
